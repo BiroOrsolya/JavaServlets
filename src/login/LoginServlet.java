b7b4e3388ec0868d75ac8d7cb036ebaccfccbf88
biroorsolya <birorso@gmail.com>
2016-03-02 13:44:44 +0100

    index.html, LoginServlet, .xml file

---
+++ src/login/LoginServlet.java
@@ -0,0 +1,25 @@
+package login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3758323642802126996L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		System.out.println("pony");
+		super.doPost(req, resp);
+	}
+
+}
