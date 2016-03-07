10851f3aada3a0c98cdf54ad68921b7673be2b7c
biroorsolya <birorso@gmail.com>
2016-03-07 12:00:13 +0100

    login

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -1,6 +1,7 @@
 package login;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,17 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet
 {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3758323642802126996L;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
-		System.out.println("pony");
-		super.doPost(req, resp);
+
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		req.getRequestDispatcher("index.html").include(req, resp);
+
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+
+		if (name.equals("pony") && password.equals("pony123"))
+			out.println("Welcome " + name);
+		else
+			out.println("Bad username or password ! Permission denied");
+		out.close();
+
+		// System.out.println("pony");
+
+		// super.doPost(req, resp);
 	}
 
 }
