26f030f2868b9e41699dd3f62fffae9fdb84cb58
biroorsolya <birorso@gmail.com>
2016-03-09 19:00:59 +0100

    Login, logout sessions, new html files

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -7,31 +7,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 3758323642802126996L;
 
-	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
 
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-		req.getRequestDispatcher("index.html").include(req, resp);
+		req.getRequestDispatcher("link.html").include(req, resp);
 
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
 
-		if (name.equals("pony") && password.equals("pony123"))
-			out.println("Welcome " + name);
-		else
+		if (password.equals("pony123"))
+		{
+			out.print("Welcome " + name);
+			HttpSession session = req.getSession();
+			session.setAttribute("name", name);
+		} else
 			out.println("Bad username or password ! Permission denied");
+		req.getRequestDispatcher("login.html");
 		out.close();
 
-		// System.out.println("pony");
-
-		// super.doPost(req, resp);
 	}
 
 }
--- src/login/LogoutServlet.java
+++ src/login/LogoutServlet.java
@@ -1,6 +1,32 @@
 package login;
 
-public class LogoutServlet
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet
 {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+
+		req.getRequestDispatcher("link.html").include(req, resp);
+
+		HttpSession session = req.getSession();
+		session.invalidate();
+
+		out.print("You are successfully logged out!");
+
+		out.close();
+	}
 
 }
